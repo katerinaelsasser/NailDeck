ee6bddd18ae0fc7546bdd2c6ab7ff93d2152a6cc
katerinaelsasser <k.elsasser@aol.co.uk>
2020-05-26 19:44:45 +0000

    changing procfile

--- NailDeck/settings.py
+++ NailDeck/settings.py
@@ -8,8 +8,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-if os.path.exists('env.py'):
-    import env
+# import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
