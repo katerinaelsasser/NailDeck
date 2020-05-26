d1b8ed3d83e7c664f1d136a690c0b568a3582a4b
katerinaelsasser <k.elsasser@aol.co.uk>
2020-05-26 18:58:38 +0000

    adjusting env py

--- NailDeck/settings.py
+++ NailDeck/settings.py
@@ -8,7 +8,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
+if os.path.exists('env.py'):
+    import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
