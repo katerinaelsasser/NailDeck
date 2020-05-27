931f47f95afa444872fbfa71cd276049833e2f82
katerinaelsasser <k.elsasser@aol.co.uk>
2020-05-27 19:17:08 +0000

    linking home

--- NailDeck/urls.py
+++ NailDeck/urls.py
@@ -25,7 +25,7 @@ from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', all_products, name='index'),
+    url(r'^$',  name='home'),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
