7e34a658b6348b838b3d7c237e645b3525e9e756
katerinaelsasser <k.elsasser@aol.co.uk>
2020-05-28 20:15:09 +0000

    updating urls

--- NailDeck/urls.py
+++ NailDeck/urls.py
@@ -25,12 +25,12 @@ from django.views import static
 from .settings import MEDIA_ROOT
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home.urls')),
-    path('accounts/', include('allauth.urls')),
-    path('products/', include('products.urls')),
-    path('cart/', include('cart.urls')),
-    path('search/', include('search.urls')),
-    path('checkout/', include('checkout.urls')),
-    path('profile/', include('profiles.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', all_products, name='index'),
+    url(r'^accounts/', include(urls_accounts)),
+    url(r'^products/', include(urls_products)),
+    url(r'^cart/', include(urls_cart)),
+    url(r'^checkout/', include(urls_checkout)),
+    url(r'^search/', include(urls_search)),
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
+]
