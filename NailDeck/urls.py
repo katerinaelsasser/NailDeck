a1244a7fd4b07e382c044693c0da2042b6264634
katerinaelsasser <k.elsasser@aol.co.uk>
2020-05-28 07:49:40 +0000

    reverting back link

--- NailDeck/urls.py
+++ NailDeck/urls.py
@@ -26,7 +26,7 @@ from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', name='home',
+    url(r'$', name='index',
     url(r'accounts/', include(urls_accounts)),
     url(r'products/', include(urls_products)),
     url(r'cart/', include(urls_cart)),
