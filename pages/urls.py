6f38f50ea445ac145770114abc4c15b4fdae7cbb
katerinaelsasser <k.elsasser@aol.co.uk>
2020-06-18 10:42:38 +0000

    removing contact page

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import url, include
-from .views import about_view, terms_view, faq_view, contact_view
+from .views import about_view, terms_view, faq_view
 
 urlpatterns = [
     url('about/', about_view, name="about"),
     url('terms-conditions/', terms_view, name="terms"),
     url('faq/', faq_view, name="faq"),
-    url('contact/', contact_view, name="contact")
-
-
 ]
