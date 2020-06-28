8583d4c240ebd8b50620885dd31eac97ddc5ce8e
katerinaelsasser <k.elsasser@aol.co.uk>
2020-06-28 20:58:38 +0000

    adding confirmation page

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import checkout
+from .views import checkout, confirmation
 
 urlpatterns = [
     url(r'^$', checkout, name='checkout'),
+    url(r'^$', confirmation, name='confirmation'),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -48,17 +48,19 @@ def checkout(request):
                 messages.error(request, "Your card was declined!")
                 
             if customer.paid:
-                messages.error(request, "You have successfully paid")
                 request.session['cart'] = {}
-                return redirect(reverse('products'))
+                return redirect(reverse('confirmation'))
             else:
-                messages.error(request, "Unable to take payment")
+                messages.error(request, "Unable to process your payment")
         else:
             print(payment_form.errors)
-            messages.error(request, "We were unable to take a payment with that card!")
+            messages.error(request, "We were unable to take payment with those card details")
     else:
-        payment_form = MakePaymentForm()
         order_form = OrderForm()
+        payment_form = PaymentForm()
         
     return render(request, "checkout.html", {'order_form': order_form, 'payment_form': payment_form, 'publishable': settings.STRIPE_PUBLISHABLE})
-                
+
+#Confirmation page
+def confirmation(request):
+    return render(request, "confirmation.html")
