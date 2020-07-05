80d23bfcac9be798e055a0e565f815a3acf09aa5
katerinaelsasser <k.elsasser@aol.co.uk>
2020-07-05 20:39:41 +0000

    removing admin url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from . import urls_reset
-from .views import register, profile, logout, login, admin_profile
+from .views import register, profile, logout, login
 urlpatterns = [
     url(r'^register/$', register, name='register'),
     url(r'^profile/$', profile, name='profile'),
