b1898ab5bc9f372fa9407c5a42b099fdbb561c8a
katerinaelsasser <k.elsasser@aol.co.uk>
2020-07-09 21:25:51 +0000

    migrating categories

--- products/models.py
+++ products/models.py
@@ -2,17 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    choices =(
-        ('nail_polishes', 'Polishes'),
-        ('nail_care', 'Care')
+    CHOICES =(
+        ('polishes', 'Polishes'),
+        ('care', 'Care')
     )
-    category = models.CharField(max_length=20, choices=choices)
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
-
-
+    category = models.CharField(max_length=11, choices=CHOICES, default='polishes')
 
     def __str__(self):
         return self.name
