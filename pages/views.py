4f00176929aac8979691cfcd03423c91a81c6927
katerinaelsasser <k.elsasser@aol.co.uk>
2020-07-13 21:24:26 +0000

    updating contact view/model

--- pages/views.py
+++ pages/views.py
@@ -16,23 +16,14 @@ def terms_view(request):
 
 #Contact page
 def contact_view(request):
-    if request.method == 'GET':
-        form = ContactForm()
-    else:
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            subject = form.cleaned_data['subject']
-            from_email = form.cleaned_data['from_email']
-            message = form.cleaned_data['message']
-            try:
-                send_mail(subject, message, from_email, ['k.elsasser@aol.co.uk'])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-            return redirect('success')
-    return render(request, "contact.html", {'form': form})
+    class ContactCreate(CreateView):
+    model = Contact
+    fields = ["first_name", "last_name", "message"]
+    success_url = reverse_lazy("thanks")
 
-def successView(request):
-    return HttpResponse('Success! Thank you for your message.')
+
+def thanks(request):
+    return HttpResponse("Thank you! Will get in touch soon.")
 
 # Your Images Page
 def social_view(request):
