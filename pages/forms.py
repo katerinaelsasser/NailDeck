89e96618ffe5386adb11713a4458152d6170f0c4
katerinaelsasser <k.elsasser@aol.co.uk>
2020-07-22 21:33:41 +0000

    chaning contact layout

--- pages/forms.py
+++ pages/forms.py
@@ -3,10 +3,7 @@ from django import forms
 class contact_form(forms.Form):
     contact_name = forms.CharField(label='Contact Name', max_length=255)
     contact_email = forms.CharField(label='Contact Email',max_length=255)
-    contact_message = forms.CharField(
-        required=True,
-        widget=forms.Textarea
-    )
+    contact_message = forms.CharField(required=True, widget=forms.Textarea)
 
 class review_form(forms.Form):
     STAR_CHOICES = [(i, i) for i in range(1, 6)]
