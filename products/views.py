e34b1d920c8a52fd236746ee8940caffe3f1c632
katerinaelsasser <k.elsasser@aol.co.uk>
2020-07-25 17:21:33 +0000

    fixing cactegory pages

--- products/views.py
+++ products/views.py
@@ -17,10 +17,6 @@ def polish_category(request, *args, **kwargs):
     """
     Displays all products that are in the polish category
     """
-     if request.method == 'POST':
-        context = get_post_request_context(request, 'Polishes')
-        return render(request, "polish.html", context)
-
     context = get_context('Polishes')
     return render(request, "polish.html", context)
 
@@ -30,24 +26,11 @@ def care_category(request, *args, **kwargs):
     """
     Displays all products that are in the care category
     """
-    if request.method == 'POST':
-        context = get_post_request_context(request, 'Care')
-        return render(request, "care.html", context)
-
     context = get_context('Care')
     return render(request, "care.html", context)
 
-def get_post_request_context(post_request, category_name):
-    context = {
-        'products': results,
-        'select': sort,
-        'category': category_name
-    }
-    return context
-
 def get_context(category_name):
     """ Returns relevant context for category views """
-    
     context = {
         'products': Product.objects.all().filter(category=category_name),
         'category': category_name
