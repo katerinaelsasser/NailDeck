e0c984db253c587720d42a4d41e7d6dd41a57a4e
katerinaelsasser <k.elsasser@aol.co.uk>
2020-07-29 20:14:41 +0000

    adding default to contact model

--- contact/models.py
+++ contact/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Contact(models.Model):
 
-    name = models.CharField(max_length=50, blank=False)
-    email = models.EmailField(max_length=200, blank=False)
-    message = models.TextField(max_length=1024)
+    name = models.CharField(max_length=50, default='test', blank=False)
+    email = models.EmailField(max_length=200, default='test', blank=False)
+    message = models.TextField(max_length=1024, default='test')
 
 
     def __str__(self):
