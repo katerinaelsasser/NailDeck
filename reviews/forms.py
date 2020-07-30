33c6ab5fe058f95549d6b66e3ac4c764b351ce27
katerinaelsasser <k.elsasser@aol.co.uk>
2020-07-30 09:23:50 +0000

    fixing review error

--- reviews/forms.py
+++ reviews/forms.py
@@ -2,8 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from .models import Review
 
-class review_form(forms.Form):
-    STAR_CHOICES = [(i, i) for i in range(1, 6)]
-
-    review_star = forms.ChoiceField(label='Review Star', choices=STAR_CHOICES, required=False)
-    review_message = forms.CharField(required=True,widget=forms.Textarea)
+class review_form(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ('review_star', 'review_message')
